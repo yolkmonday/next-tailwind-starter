d5e0204359aa61576b1956d46fa92c4475c35273
aripadrian <aripadrian01@gmail.com>
2023-03-30 10:32:29 +0700

    add tailwind

---
+++ src/pages/index.jsx
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Create Next App</title>
+        
+      </Head>
+      <main>
+        tes
+      </main>
+
+    </>
+  )
+}
